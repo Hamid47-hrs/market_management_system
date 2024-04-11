c3e597f1f7cdcc5262d64a630655266733f06f92
Hamid47hrs <hamid47.hrs@gmail.com>
2024-04-11 23:10:53 +0330

    Implement 'Transaction' page for search records.

---
+++ Controllers/TransactionsController.cs
@@ -0,0 +1,28 @@
+﻿using market_management_system.Models;
+using market_management_system.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace market_management_system.Controllers;
+
+public class TransactionsController : Controller
+{
+    public IActionResult Index()
+    {
+        TransactionsViewModel transactionsViewModel = new TransactionsViewModel();
+
+        return View(transactionsViewModel);
+    }
+
+    public IActionResult Search(TransactionsViewModel transactionsViewModel)
+    {
+        var transactions = TransactionsRepository.Search(
+            transactionsViewModel.CashierName ?? "",
+            transactionsViewModel.StartDate,
+            transactionsViewModel.EndDate
+        );
+
+        transactionsViewModel.Transactions = transactions;
+
+        return View("Index", transactionsViewModel);
+    }
+}
