fe401b026d3a0a13726bb60214a42d3fa3076b7d
Hamid47hrs <hamid47.hrs@gmail.com>
2024-04-18 15:23:29 +0330

    Create a separate project for the application models.

---
+++ CoreBusiness/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreBusiness;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Category")]
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+
+    [Required]
+    public string Name { get; set; } = "";
+
+    [Required]
+    public double Price { get; set; }
+
+    [Required]
+    public int? Quantity { get; set; }
+}
