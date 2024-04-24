4cffb2d2f2341597822eff17e853e529fe5bfaac
Hamid47hrs <hamid47.hrs@gmail.com>
2024-04-24 22:49:01 +0330

    Implement the 'Transaction' SQL repository.

---
+++ Plugins/Plugins.DataStore.SQL/Migrations/TransactionSQLRepository.cs
@@ -0,0 +1,71 @@
+﻿using CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.SQL;
+
+public class TransactionSQLRepository : ITransactionRepository
+{
+    private readonly MarketContext db;
+
+    public TransactionSQLRepository(MarketContext db)
+    {
+        this.db = db;
+    }
+
+    public void Add(
+        int productId,
+        string productName,
+        double price,
+        int? qtyBefore,
+        int qtySold,
+        string cashier
+    )
+    {
+        var transaction = new Transaction()
+        {
+            ProductId = productId,
+            ProductName = productName,
+            TimeStamp = DateTime.Now,
+            Price = price,
+            QtyBefore = qtyBefore,
+            QtySold = qtySold,
+            CashierName = cashier
+        };
+
+        db.Transactions.Add(transaction);
+        db.SaveChanges();
+    }
+
+    public IEnumerable<Transaction> ReadByDateAndCashier(DateTime date, string cashier)
+    {
+        if (string.IsNullOrWhiteSpace(cashier))
+        {
+            return db.Transactions.Where(x => x.TimeStamp == date.Date);
+        }
+        else
+        {
+            return db.Transactions.Where(x =>
+                EF.Functions.Like(x.CashierName, $"%{cashier}%") && x.TimeStamp == date.Date
+            );
+        }
+    }
+
+    public IEnumerable<Transaction> Search(string cashier, DateTime startDate, DateTime endDate)
+    {
+        if (string.IsNullOrWhiteSpace(cashier))
+        {
+            return db.Transactions.Where(x =>
+                x.TimeStamp >= startDate.Date && x.TimeStamp <= endDate.Date
+            );
+        }
+        else
+        {
+            return db.Transactions.Where(x =>
+                EF.Functions.Like(x.CashierName, $"%{cashier}%")
+                && x.TimeStamp >= startDate.Date
+                && x.TimeStamp <= endDate.Date
+            );
+        }
+    }
+}
